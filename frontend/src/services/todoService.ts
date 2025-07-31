9b964ecb9fae8e09484cd3fdaef9278d49f2b139
David Johnston <david@blacksheepcode.com>
2025-07-31 14:41:58 +1000

    Add useProfile

--- frontend/src/services/todoService.ts
+++ frontend/src/services/todoService.ts
@@ -4,11 +4,6 @@ export type Todo = {
     completed: boolean;
 }
 
-export type Todo = {
-    id: number; 
-    title: string; 
-    completed: boolean;
-}
 
 export async function fetchTodos() : Promise<Array<Todo>>{
   const res = await fetch('http://localhost:3000/v1/todos');
@@ -16,11 +11,7 @@ export async function fetchTodos() : Promise<Array<Todo>>{
   return res.json();
 }
 
-export async function getProfile() {
-  const res = await fetch('http://localhost:3000/v1/profile');
-  if (!res.ok) throw new Error('Network response was not ok');
-  return res.json();
-}
+
 
 export async function getTodosV2() {
   const res = await fetch('http://localhost:3000/v2/todos');
